b17334196f13eccf7c1e9bca7de31839e80f67a2
JinuEngels <jinuengels23@winchendon.org>
2021-12-08 10:12:56 -0500

    This is the code for activities 2.6

---
+++ Homework/11-29-21.js
@@ -0,0 +1,30 @@
+var myName = "Jinu Engels";
+if (myName === "Jinu Engels") {
+    console.log("Hello, me!")
+}
+else {
+    console.log("Hello, stranger.")
+}
+
+var myName = "Kenny Engels";
+if (myName === "Kenny Engels") {
+    console.log("Hey, Dad!")
+}
+else if (myName === "Unha Engels") {
+    console.log("Hey, Mom!")
+}
+else {
+    console.log("Hello, stranger.")
+}
+
+document.getElementById("demo").innerHTML = "Good day!";
+new Date().getHours();
+if (time < 10) {
+    console.log("Good morning!")
+}
+else if (time > 10 < 20) {
+    console.log("Good day!")
+}
+else {
+    console.log("Good evening!")
+}
